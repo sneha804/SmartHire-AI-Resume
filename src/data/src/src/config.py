bb5f511bf31ebd44d077d360a85e04f6f90a2d51
sneha804 <snehapotluri07@gmail.com>
2026-07-15 19:17:07 +0530

    Create config.py

---
+++ src/data/src/src/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+RAW_DATA = BASE_DIR / "data" / "raw"
+
+PROCESSED_DATA = BASE_DIR / "data" / "processed"
+
+MODEL_PATH = BASE_DIR / "models"
+
+ASSET_PATH = BASE_DIR / "app" / "assets"
